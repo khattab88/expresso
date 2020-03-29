b90d99d0961fd6ef3620b1514a19a64656a4268e
unknown <m_khatab_88@hotmail.com>
2020-03-29 15:05:50 +0200

    configure html webpack plugin

--- assets/js/nav-toggle.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    $(".nav-toggle").click(function(){
-        let nav = $(".nav-container");
-        nav.slideToggle();
-    });
-
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/js/index.js",
@@ -8,5 +9,11 @@ module.exports = {
     },
     devServer: {
         contentBase: "."
-    }
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: "index.html",
+            template: "./index.html"
+        })
+    ]
 };
