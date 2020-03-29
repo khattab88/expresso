455d473e24c59aa56ef886fda67f43d82860dba6
unknown <m_khatab_88@hotmail.com>
2020-03-29 15:43:04 +0200

    copying pages html files to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,30 @@ module.exports = {
         new htmlWebpackPlugin({
             filename: "index.html",
             template: "./index.html"
-        })
+        }),
+        new htmlWebpackPlugin({
+            filename: "restaurant-list/index.html",
+            template: "./restaurant-list/index.html"
+        }),
+        new htmlWebpackPlugin({
+            filename: "restaurant-menu/index.html",
+            template: "./restaurant-menu/index.html"
+        }),
+        new htmlWebpackPlugin({
+            filename: "login/index.html",
+            template: "./login/index.html"
+        }),
+        new htmlWebpackPlugin({
+            filename: "sign-up/index.html",
+            template: "./sign-up/index.html"
+        }),
+        new htmlWebpackPlugin({
+            filename: "contact-us/index.html",
+            template: "./contact-us/index.html"
+        }),
+        new htmlWebpackPlugin({
+            filename: "checkout/index.html",
+            template: "./checkout/index.html"
+        }),
     ]
 };
