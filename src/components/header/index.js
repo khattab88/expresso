032cd58aa4914be36408e9e98a1e63db872232fa
unknown <m_khatab_88@hotmail.com>
2020-04-04 16:19:40 +0200

    header restaurant selection (restaurant list)

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -21,5 +21,21 @@ export default class HeaderComponent {
             elements.locationSelectionBox.classList.toggle("location-selection__box--open");
         });
 
+
+         // toggle header restaurant selection
+        elements.restaurantSelectionBtn.addEventListener("click", e => {
+            elements.restaurantSelectionBtn.classList.toggle("restaurant-selection__btn--open");
+            elements.restaurantSelectionBox.classList.toggle("restaurant-selection__box--open");
+            console.log(elements.restaurantSelectionBtn);
+        });
+
+         // display restaurant location value
+        elements.restaurantList.addEventListener("click", e => {
+            const area = e.target.textContent;
+            elements.restaurantValue.textContent = area;
+            elements.restaurantSelectionBtn.classList.toggle("restaurant-selection__btn--open");
+            elements.restaurantSelectionBox.classList.toggle("restaurant-selection__box--open");
+        });
+
     }
 }
--- src/components/header/view.js
+++ src/components/header/view.js
@@ -3,4 +3,8 @@ export const elements = {
     locationSelectionBox: document.querySelector(".location-selection__box"),
     locationList: document.querySelector(".location-selection__city-list"),
     locationValue: document.querySelector(".location-selection__value"),
+    restaurantSelectionBtn: document.querySelector(".restaurant-selection__btn"),
+    restaurantSelectionBox: document.querySelector(".restaurant-selection__box"),
+    restaurantList: document.querySelector(".restaurant-selection__restaurant-list"),
+    restaurantValue: document.querySelector(".restaurant-selection__value"),
 };
