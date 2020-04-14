32100c743841c526120d5ac2201cb6dfcaf74998
unknown <m_khatab_88@hotmail.com>
2020-04-14 05:28:22 +0200

    render footer component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,16 +1,30 @@
- import { elements } from "./view";
+ import { elements, FooterView } from "./view";
 
  export default class FooterComponent {
     constructor() {
         this.setupEventListeners();
     }
 
+    init() {
+        this.view = new FooterView();
+
+        this.view.render();
+    }
+
     setupEventListeners() {
         
-        // toggle footer country
-        elements.footerCountryBtn.addEventListener("click", e => {
-            elements.footerCountryBtn.classList.toggle("change-country__btn--open");
-            elements.footerCountryDropdown.classList.toggle("country-dropdown--open");
+        window.addEventListener("load", this.init);
+
+        elements.container.addEventListener("click", e => {
+
+            // toggle footer country
+            if(e.target.matches(".change-country__btn, .change-country__btn *")) {
+                const footerCountryBtn = elements.container.querySelector(".change-country__btn");
+                footerCountryBtn.classList.toggle("change-country__btn--open");
+
+                const footerCountryDropdown = elements.container.querySelector(".change-country__btn > .country-dropdown");
+                footerCountryDropdown.classList.toggle("country-dropdown--open");
+            }
         });
 
     }
