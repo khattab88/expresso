c8b49142ccecd70e6b724f6af5d1915671ce9eca
unknown <m_khatab_88@hotmail.com>
2020-04-20 12:15:12 +0200

    fix close item modal

--- src/components/menu-item-modal/index.js
+++ src/components/menu-item-modal/index.js
@@ -16,14 +16,19 @@ export default class ItemModalComponent {
         try {
 
             // open modal
-            elements.menuItemsWrapper.addEventListener("click", e => this.view.open(e));
+            elements.menuItemsWrapper.addEventListener("click", e => this.view.open(e),);
 
             // close modal
-            document.querySelectorAll(".menu-item-modal__close, .add-order-btn, .menu-item-modal")
+            document.querySelectorAll(".menu-item-modal__close, .add-order-btn")
                 .forEach(elm => elm.addEventListener("click", this.view.close));
 
+
             // toggle item option
-            elements.modalBody.addEventListener("click", e => this.view.toggleItemOption(e));
+            elements.modalBody.addEventListener("click", e => { 
+                if(e.target.matches(".dish-options__head, .dish-options__head *")) {
+                    this.view.toggleItemOption(e);
+                }
+            });
 
             // increment/decrement menu item count
             document.querySelectorAll(".cart-controls")
