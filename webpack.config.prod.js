f2b456341aadf61292df41a0e99a4458eed90764
unknown <m_khatab_88@hotmail.com>
2020-05-02 22:18:49 +0200

    generate dynamic name for production bundle file

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: ["babel-polyfill", "./src/index.js"],
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename: "[contenthash].js",
         // publicPath: "dist/" 
     },
     devtool: "cheap-source-map",
