2fb6087f701c22f6cc873fe29917f82836f3e44b
unknown <m_khatab_88@hotmail.com>
2020-05-02 20:41:28 +0200

    add webpack public path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
     entry: ["babel-polyfill", "./src/index.js"],
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: "dist/assets/"
     },
     devServer: {
         contentBase: "./dist"
