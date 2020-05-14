8e4d01195ad5b2eb20f452d46b6c302b3f95be20
unknown <m_khatab_88@hotmail.com>
2020-05-15 00:30:59 +0200

    implement publisher subscriber module

---
+++ src/lib/pubsub.js
@@ -0,0 +1,41 @@
+/* eslint-disable newline-after-var */
+/* eslint-disable indent-legacy */
+/* eslint-disable indent */
+/* eslint-disable no-undef */
+/* eslint-disable max-classes-per-file */
+
+/* eslint-disable indent-legacy */
+/* eslint-disable indent */
+/* eslint-disable curly */
+class PubSub {
+
+    constructor () {
+        // this.subscribers = {};
+
+        // PubSub.subscribers = {};
+    }
+
+    static subscribers = {};
+
+    static publish (event, data) {
+        if (!PubSub.subscribers[event]) return;
+
+        PubSub.subscribers[event].forEach(subscriberCallback => subscriberCallback(data));
+    }
+
+    static subscribe (event, callback) {
+        let index = 0;
+
+        if (!PubSub.subscribers[event]) {
+            PubSub.subscribers[event] = [];
+        }
+
+        index = PubSub.subscribers[event].push(callback) - 1;
+
+        return {
+            unsubscribe () {
+                PubSub.subscribers[event].splice(index, 1);
+            }
+        };
+    }
+}
